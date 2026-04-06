36f9961a3f6e1b554a8ff06c9d6b9bbb1e8d8607
khalil <medkhalilbs@gmail.com>
2026-04-06 20:19:41 +0100

    Init CI github

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Artemis2_FAQ/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
